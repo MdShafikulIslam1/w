24aa91b85ec60b62ed7f5354102737d7cc9fdebc
shafikul-islam <179398ssshofik@gmail.com>
2023-12-29 15:37:10 +0600

    [DONE] retrieve all messages

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,13 +1,13 @@
 import getPrismaInstance from "../utils/PrismaClient.js";
 
 export const addMessage = async (req, res, next) => {
-  console.log(req.body)
+  console.log(req.body);
   try {
     const prisma = getPrismaInstance();
     const { message, from, to } = req.body;
     const getUser = onlineUsers.get(to);
     if (message && from && to) {
-      console.log(req.body,"valid")
+      console.log(req.body, "valid");
       const newMessage = await prisma.message.create({
         data: {
           message,
@@ -25,6 +25,53 @@ export const addMessage = async (req, res, next) => {
     return res
       .status(400)
       .send({ message: "Message, from and to must be provided" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMessages = async (req, res, next) => {
+  try {
+    const { from, to } = req.params;
+    const prisma = getPrismaInstance();
+    const messages = await prisma.message.findMany({
+      where: {
+        OR: [
+          {
+            senderId: parseInt(from),
+            receiverId: parseInt(to),
+          },
+          {
+            senderId: parseInt(to),
+            receiverId: parseInt(from),
+          },
+        ],
+      },
+      orderBy: {
+        id: "asc",
+      },
+    });
+    const unreadMessages = [];
+    messages.forEach((message, index) => {
+      if (
+        message?.messageStatus !== "read" &&
+        message?.senderId === parseInt(to)
+      ) {
+        messages[index] = "read";
+        unreadMessages.push(message?.id);
+      }
+    });
+    await prisma.message.updateMany({
+      where: {
+        id: {
+          in: unreadMessages,
+        },
+      },
+      data: {
+        messageStatus: "read",
+      },
+    });
+    return res.status(200).json({ messages });
   } catch (error) {
     next(error);
   }
